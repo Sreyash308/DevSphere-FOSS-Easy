db391e945c7ac92f89ea9f15f24da104d9fc1eaa
Vennela Varshini Anasoori <vennelavarshini07@gmail.com>
2026-03-27 21:16:08 +0530

    I feel this is the correct commit

--- solution1.cpp
+++ solution1.cpp
@@ -6,7 +6,12 @@ int main() {
     while(t--){
         long long n, k;
         cin >> n >> k;
-        cout << k + (k / n) << endl; 
+        long long count = 0, current = 0;
+        while (count < k) {
+            current++;
+            if (current % n != 0) count++;
+        }
+        cout << current << endl;
     }
     return 0;
 }
